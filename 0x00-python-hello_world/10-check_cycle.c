e9493a975a2094c5178d387c4eec4e202343de40
tomiwa <codertomiwa@gmail.com>
2024-01-11 05:27:26 -0800

    10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * check_cycle - funtion to check if a singly linked list has a cycle in it
+ * @list: linked list
+ * Return: 0 or 1
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *fast;
+	listint_t *slow;
+
+	if (!list || !list->next)
+		return (0);
+	fast = list;
+	slow = list;
+
+	while (slow != NULL && fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+		return (1);
+		}
+	}
+	return (1);
+}
