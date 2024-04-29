9eb58c4374243283e492e0ec5a0bb95d0e288658
Tomiwa <codertomiwa@gmail.com>
2024-04-29 03:54:15 -0700

    6-peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,4 +6,4 @@ def find_peak(list_of_integers):
     """Finds a peak in a list of unsorted integers."""
     if list_of_integers:
         list_of_integers.sort(reverse=True)
-        return list_of_integers[0]      
+        return list_of_integers[0]
