feae05ae3b85e195075ecd8c9a9197f0a2188455
Tomiwa <codertomiwa@gmail.com>
2024-05-29 21:50:54 +0100

    102-script.js

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,10 @@
+// fetches and prints how to say "Hello" depending on language
+
+$(document).ready(function() {
+    $("#btn_translate").click(function() {
+        const langCode = $("#language_code").val();
+        $.get(`https://www.fourtonfish.com/hellosalut/hello/?lang=${langCode}`, function(data) {
+            $("#hello").text(data.hello);
+        });
+    });
+});
